744b30162999b607782511148792788911e0982a
swenmeeuwes <swenmeeu@gmail.com>
2015-12-14 23:23:14 +0100

    ADDED: Get(unitId)
    USAGE: "api/maintenance/<unitId>"

--- DataflowAnalyseWebApp/DataflowAnalyseWebApp/Controllers/MaintenanceControllers.cs
+++ DataflowAnalyseWebApp/DataflowAnalyseWebApp/Controllers/MaintenanceControllers.cs
@@ -15,43 +15,50 @@ namespace DataflowAnalyseWebApp.Controllers
     public class MaintenanceController : ApiController
     {
         // GET api/values
-        public IEnumerable<Position> Get()
+        public IEnumerable<Maintenance> Get()
+        {
+            return new List<Maintenance>();
+        }
+
+        // GET api/maintenance/5
+        public Maintenance Get(int unitId)
         {
             string webserviceUrl = WebConfigurationManager.AppSettings["WebserviceUrl"];
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(webserviceUrl + "/positions");
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(webserviceUrl + "/positions/" + unitId.ToString());
             HttpWebResponse response = (HttpWebResponse)request.GetResponse();
             Stream receiveStream = response.GetResponseStream();
             StreamReader readStream = new StreamReader(receiveStream, Encoding.UTF8);
             string responseString = readStream.ReadToEnd();
             PositionResponse positionResponse = JsonConvert.DeserializeObject<PositionResponse>(responseString);
 
-            double d1 = distance(32.9697, -96.80322, 29.46786, -98.53506);
+            Maintenance maintenance = new Maintenance();
+            maintenance.unitId = unitId;
 
-            return positionResponse.result;
+            double travelled = 0;
+            for (int i = 0; i < positionResponse.result.Length - 1; i++)
+            {
+                travelled += CalcDistance(positionResponse.result[i].latitudeGps, positionResponse.result[i].longitudeGps, positionResponse.result[i+1].latitudeGps, positionResponse.result[i+1].longitudeGps);
+            }
+
+            return maintenance;
         }
 
-        // GET api/values/5
-        public string Get(int unitId)
-        {
-            return "value";
-        }
-
-        private double distance(double lat1, double lon1, double lat2, double lon2)
+        private double CalcDistance(double lat1, double lon1, double lat2, double lon2)
         {
             double theta = lon1 - lon2;
-            double dist = Math.Sin(deg2rad(lat1)) * Math.Sin(deg2rad(lat2)) + Math.Cos(deg2rad(lat1)) * Math.Cos(deg2rad(lat2)) * Math.Cos(deg2rad(theta));
+            double dist = Math.Sin(DegToRad(lat1)) * Math.Sin(DegToRad(lat2)) + Math.Cos(DegToRad(lat1)) * Math.Cos(DegToRad(lat2)) * Math.Cos(DegToRad(theta));
             dist = Math.Acos(dist);
-            dist = rad2deg(dist);
+            dist = RadToDeg(dist);
             dist = dist * 1.609344;
             return (dist);
         }
 
-        private double deg2rad(double deg)
+        private double DegToRad(double deg)
         {
             return (deg * Math.PI / 180.0);
         }
 
-        private double rad2deg(double rad)
+        private double RadToDeg(double rad)
         {
             return (rad / Math.PI * 180.0);
         }
