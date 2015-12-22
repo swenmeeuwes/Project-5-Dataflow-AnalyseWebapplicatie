3e845aff353d9a6d4593b0fcb98fd2805c00f88e
gvh1993 <gertjanvanh@live.nl>
2015-12-22 15:20:15 +0100

    cleanup monitoring model

--- DataflowAnalyseWebApp/DataflowAnalyseWebApp/Models/Monitoring/Monitoring.cs
+++ DataflowAnalyseWebApp/DataflowAnalyseWebApp/Models/Monitoring/Monitoring.cs
@@ -18,14 +18,5 @@ namespace DataflowAnalyseWebApp.Models.Monitoring
         public double minValue { get; set; }
         public double maxValue { get; set; }
         public double sumValue { get; set; }
-
-        ////added for analysis
-        //public double percentUsed { get; set; }
-        //public string diskSpaceStatus { get; set; }
-
-        public override string ToString()
-        {
-            return string.Format("{0} {1} {2} {3} {4} {5} {6} ", unitId, beginTime, endTime, sensorType, minValue, maxValue, sumValue);
-        }
     }
 }
