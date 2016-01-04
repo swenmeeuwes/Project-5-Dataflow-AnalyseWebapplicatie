e72d204fcfe69c1ed7dd233850bee25ffb5b367e
swenmeeuwes <swenmeeu@gmail.com>
2016-01-04 22:11:50 +0100

    Added: UnitTest
    Testing GetDistanceTo

---
+++ DataflowAnalyseWebApp/DataflowAnalyseWebAppTests/Controllers/MaintenanceControllerTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DataflowAnalyseWebApp.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Device.Location;
+
+namespace DataflowAnalyseWebApp.Controllers.Tests
+{
+    [TestClass()]
+    public class MaintenanceControllerTests
+    {
+        Random random = new Random();
+        [TestMethod()]
+        public void GetDistance()
+        {
+            GeoCoordinate position1 = new GeoCoordinate(random.Next(180) - 90, random.Next(360) - 180);
+            GeoCoordinate position2 = new GeoCoordinate(random.Next(180) - 90, random.Next(360) - 180);
+            Assert.AreNotEqual(Double.NaN, position1.GetDistanceTo(position2));
+        }
+    }
+}
