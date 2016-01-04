21105ba3a3ae04a9bdd7c79a1840c40dc16218d9
swenmeeuwes <swenmeeu@gmail.com>
2016-01-04 22:48:25 +0100

    Cleaned code

---
+++ DataflowAnalyseWebApp/DataflowAnalyseWebApp/Controllers/Database/DatabaseController.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+namespace DataflowAnalyseWebApp.Controllers.Database
+{
+    [Obsolete]
+    public class DatabaseController
+    {
+        public MongoDatabase database { get; private set; }
+
+        public DatabaseController()
+        {
+            MongoServerSettings settings = new MongoServerSettings();
+            settings.Server = new MongoServerAddress(WebConfigurationManager.AppSettings["DatabaseIp"], Convert.ToInt32(WebConfigurationManager.AppSettings["DatabasePort"]));
+
+            MongoServer server = new MongoServer(settings);
+
+            database = server.GetDatabase("Dataflow");
+        }
+    }
+}
--- DataflowAnalyseWebApp/DataflowAnalyseWebApp/Controllers/MaintenanceControllers.cs
+++ DataflowAnalyseWebApp/DataflowAnalyseWebApp/Controllers/MaintenanceControllers.cs
@@ -1,4 +1,5 @@
-﻿using DataflowAnalyseWebApp.Models;
+﻿using DataflowAnalyseWebApp.Controllers.Database;
+using DataflowAnalyseWebApp.Models;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
 using MongoDB.Driver;
@@ -24,7 +25,7 @@ namespace DataflowAnalyseWebApp.Controllers
         // GET api/values
         public IEnumerable<Maintenance> Get()
         {
-            MongoDatabase database = new DBController2().database;
+            MongoDatabase database = new DatabaseController().database;
             IEnumerable<BsonValue> bsonValues = database.GetCollection<Position>("positions").Distinct("unitId");
             List<long> uniqueUnitIds = BsonSerializer.Deserialize<List<long>>(bsonValues.ToJson());
 
@@ -49,12 +50,19 @@ namespace DataflowAnalyseWebApp.Controllers
             return GetMaintenanceFromUnitId(unitId);
         }
 
+        private List<T> GetCollection<T>(string collectionName, IMongoQuery query)
+        {
+            MongoDatabase database = new DatabaseController().database;
+            return database.GetCollection<T>(collectionName).Find(query).ToList();
+        }
+        private List<T> GetCollection<T>(MongoDatabase database, string collectionName, IMongoQuery query)
+        {
+            return database.GetCollection<T>(collectionName).Find(query).ToList();
+        }
+
         private Maintenance GetMaintenanceFromUnitId(long unitId)
         {
-            MongoDatabase database = new DBController2().database;
-            IMongoQuery query = Query<Position>.EQ(p => p.unitId, unitId);
-            List<Position> positions = database.GetCollection<Position>("positions").Find(query).ToList();
-
+            List<Position> positions = GetCollection<Position>("positions", Query<Position>.EQ(p => p.unitId, unitId));
 
             Maintenance maintenance = new Maintenance();
             maintenance.unitId = unitId;
@@ -72,9 +80,7 @@ namespace DataflowAnalyseWebApp.Controllers
 
         private Maintenance GetMaintenanceFromUnitId(MongoDatabase database, long unitId)
         {
-            IMongoQuery query = Query<Position>.EQ(p => p.unitId, unitId);
-            List<Position> positions = database.GetCollection<Position>("positions").Find(query).ToList();
-
+            List<Position> positions = GetCollection<Position>(database, "positions", Query<Position>.EQ(p => p.unitId, unitId));
 
             Maintenance maintenance = new Maintenance();
             maintenance.unitId = unitId;
@@ -82,46 +88,12 @@ namespace DataflowAnalyseWebApp.Controllers
             double travelled = 0;
             for (int i = 0; i < positions.Count - 1; i++)
             {
-                travelled += CalcDistance(positions[i].latitudeGps, positions[i].longitudeGps, positions[i + 1].latitudeGps, positions[i + 1].longitudeGps);
+                GeoCoordinate position1 = new GeoCoordinate(positions[i].latitudeGps, positions[i].longitudeGps);
+                GeoCoordinate position2 = new GeoCoordinate(positions[i + 1].latitudeGps, positions[i + 1].longitudeGps);
+                travelled += position1.GetDistanceTo(position2) / 1000;
             }
             maintenance.kilometersTravelled = Math.Round(travelled, 2);
             return maintenance;
-        }
-        [Obsolete]
-        private double CalcDistance(double lat1, double lon1, double lat2, double lon2)
-        {
-            double theta = lon1 - lon2;
-            double dist = Math.Sin(DegToRad(lat1)) * Math.Sin(DegToRad(lat2)) + Math.Cos(DegToRad(lat1)) * Math.Cos(DegToRad(lat2)) * Math.Cos(DegToRad(theta));
-            dist = Math.Acos(dist);
-            dist = RadToDeg(dist);
-            dist = dist * 60 * 1.1515;
-            dist = dist * 1.609344;
-            return (dist);
-        }
-        [Obsolete]
-        private double DegToRad(double deg)
-        {
-            return (deg * Math.PI / 180.0);
-        }
-        [Obsolete]
-        private double RadToDeg(double rad)
-        {
-            return (rad / Math.PI * 180.0);
-        }
-    }
-
-    internal class DBController2
-    {
-        public MongoDatabase database { get; private set; }
-
-        public DBController2()
-        {
-            MongoServerSettings settings = new MongoServerSettings();
-            settings.Server = new MongoServerAddress("145.24.222.160", 8010);
-
-            MongoServer server = new MongoServer(settings);
-
-            database = server.GetDatabase("Dataflow");
         }
     }
 }
